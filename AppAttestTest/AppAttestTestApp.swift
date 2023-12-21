4d5c63b645c4524bb112934df0183c9c2e3688ad
Pushp Abrol <pushp.abrol@gmail.com>
2023-12-21 18:20:55 -0500

    Initial Commit

---
+++ AppAttestTest/AppAttestTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppAttestTestApp.swift
+//  AppAttestTest
+//
+//  Created by Pushp Abrol on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct AppAttestTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
